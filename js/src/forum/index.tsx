a8e39c62bf1bb516c18ec9d7b9160eb662fc928c
huoxin <23447157+huoxin233@users.noreply.github.com>
2026-07-03 01:40:14 +0800

    fix: use strict equality for boolean setting evaluation

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -17,7 +17,7 @@ app.initializers.add('huoxin/money-with-history', () => {
     const user = (this.attrs as { user: User }).user;
     const money = user.attribute<number>('money');
 
-    if (app.forum.attribute('huoxin-money-with-history.hide_zero_balances') == 1) {
+    if (app.forum.attribute('huoxin-money-with-history.hide_zero_balances') === true) {
       if (money !== 0) {
         items.add('money', <span>{moneyName.replace('[money]', String(money))}</span>);
       }
